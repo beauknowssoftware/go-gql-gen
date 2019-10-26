ecc913015384ca2b0db5303f1fac56624a8f0021
Beau Bennett <beau.bennett@beauknowssoftware.com>
2019-10-26 18:36:27 -0400

    Generate param args

--- internal/gengqlinputs/main.go
+++ internal/gengqlinputs/main.go
@@ -151,4 +151,62 @@ func Run() {
 		}
 		return true
 	})
+
+	parse.Traverse(rnode, func(n parse.Node) bool {
+		if tdn, ok := n.(parse.TypeDefNode); ok {
+			if tdn.Input {
+				return false
+			}
+			parse.Traverse(tdn, func(n parse.Node) bool {
+				if fn, ok := n.(parse.FieldNode); ok {
+					if len(fn.Params) == 0 {
+						return false
+					}
+
+					fmt.Println()
+					fmt.Printf("type %v%vArgs struct {\n", tdn.Name, strings.Title(fn.Name))
+					for _, n := range fn.Params {
+						pn := n.(parse.ParamNode)
+						tn := pn.Type.(parse.TypeNode)
+						if strings.HasSuffix(pn.Name, "Id") {
+							prefix := strings.TrimSuffix(pn.Name, "Id")
+							fmt.Printf("\t%vID", strings.Title(prefix))
+						} else if pn.Name == "id" {
+							fmt.Print("\tID")
+						} else {
+							fmt.Printf("\t%v", strings.Title(pn.Name))
+						}
+						switch tn.Name {
+						case "String":
+							if tn.Multiple {
+								fmt.Print(" []string")
+							} else {
+								fmt.Print(" string")
+							}
+						case "Int":
+							if tn.Multiple {
+								fmt.Print(" []int")
+							} else {
+								fmt.Print(" int")
+							}
+						case "ID":
+							fmt.Print(" ID")
+						default:
+							if tn.Multiple {
+								fmt.Printf(" []%v", tn.Name)
+							} else {
+								fmt.Printf(" %v", tn.Name)
+							}
+						}
+						fmt.Printf(" `json:\"%v\"`", pn.Name)
+						fmt.Println()
+					}
+					fmt.Println("}")
+				}
+				return true
+			})
+			return false
+		}
+		return true
+	})
 }
