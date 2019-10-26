da51b22a69cc07b2e6dc42774f15f78ebd2e1616
Beau Bennett <beau.bennett@beauknowssoftware.com>
2019-10-26 17:50:12 -0400

    Handle resolver types in gen gql inputs

--- internal/gengqlinputs/main.go
+++ internal/gengqlinputs/main.go
@@ -12,6 +12,15 @@ import (
 
 var packageFlag = flag.String("package", "", "")
 
+func hasResolveDirective(fn parse.FieldNode) bool {
+	for _, n := range fn.Directives {
+		if dn, ok := n.(parse.DirectiveNode); ok && dn.Name == "resolve" {
+			return true
+		}
+	}
+	return false
+}
+
 func Run() {
 	flag.Parse()
 
@@ -68,6 +77,65 @@ func Run() {
 							fmt.Printf(" []%v", tn.Name)
 						} else if tn.Name == "ID" {
 							fmt.Print(" ID")
+						} else {
+							fmt.Printf(" *%v", tn.Name)
+						}
+					}
+					fmt.Printf(" `json:\"%v\"`", fn.Name)
+					fmt.Println()
+					return false
+				}
+				return true
+			})
+			fmt.Println("}")
+			return false
+		}
+		return true
+	})
+
+	parse.Traverse(rnode, func(n parse.Node) bool {
+		if tdn, ok := n.(parse.TypeDefNode); ok {
+			if tdn.Input || tdn.Name == "Mutation" || tdn.Name == "Query" {
+				return false
+			}
+			fmt.Println()
+			fmt.Printf("type %v struct {\n", tdn.Name)
+			parse.Traverse(tdn, func(n parse.Node) bool {
+				if fn, ok := n.(parse.FieldNode); ok {
+					tn := fn.Type.(parse.TypeNode)
+					if tn.Name == "Query" {
+						return false
+					}
+					if hasResolveDirective(fn) {
+						fmt.Printf("\t%vLink\n", tn.Name)
+						return false
+					}
+					if strings.HasSuffix(fn.Name, "Id") {
+						prefix := strings.TrimSuffix(fn.Name, "Id")
+						fmt.Printf("\t%vID", strings.Title(prefix))
+					} else if fn.Name == "id" {
+						fmt.Print("\tID")
+					} else {
+						fmt.Printf("\t%v", strings.Title(fn.Name))
+					}
+					switch tn.Name {
+					case "String":
+						if tn.Multiple {
+							fmt.Print(" []string")
+						} else {
+							fmt.Print(" string")
+						}
+					case "Int":
+						if tn.Multiple {
+							fmt.Print(" []int")
+						} else {
+							fmt.Print(" int")
+						}
+					case "ID":
+						fmt.Print(" ID")
+					default:
+						if tn.Multiple {
+							fmt.Printf(" []%v", tn.Name)
 						} else {
 							fmt.Printf(" *%v", tn.Name)
 						}
